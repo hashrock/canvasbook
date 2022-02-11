2524200e76f0ef4d4a104c66dbefa694b9c22fd1
hashrock <hashrock@users.noreply.github.com>
2022-02-12 00:06:21 +0900

    vue2

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # canvasbook
 
 入門 Webお絵かきツールのサンプルコード
+
+# 下書き
+
+https://scrapbox.io/sushitecture/%E3%81%8A%E7%B5%B5%E3%81%8B%E3%81%8D%E3%83%84%E3%83%BC%E3%83%AB%E3%82%92%E4%BD%9C%E3%82%8B%E8%AC%9B%E5%BA%A7
---
+++ vue/main.js
@@ -0,0 +1,66 @@
+
+let ctx = null;
+let canvas = null;
+
+export default {
+  data() {
+    return {
+      drag: false,
+      old: null,
+      eraser: false,
+      color: "black",
+      lineWidth: 1
+    };
+  },
+  watch: {
+    eraser(n) {
+      ctx.globalCompositeOperation = n ? "destination-out" : "source-over";
+    },
+    lineWidth(w) {
+      ctx.lineWidth = w;
+    }
+  },
+  methods: {
+    down(ev) {
+      canvas.setPointerCapture(ev.pointerId);
+      this.drag = true;
+      this.old = {
+        x: ev.offsetX,
+        y: ev.offsetY
+      };
+    },
+    up() {
+      this.drag = false;
+    },
+    move(ev) {
+      if (this.drag) {
+        ctx.beginPath();
+        ctx.strokeStyle = this.color;
+        ctx.moveTo(this.old.x, this.old.y);
+        ctx.lineTo(ev.offsetX, ev.offsetY);
+        ctx.stroke();
+        // ctx.closePath();
+        this.old = {
+          x: ev.offsetX,
+          y: ev.offsetY
+        };
+      }
+    },
+    clear() {
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+    },
+    save() {
+      const a = document.createElement("a");
+      a.href = canvas.toDataURL("image/png");
+      a.download = "download.png";
+      a.click();
+    }
+  },
+  mounted() {
+    canvas = this.$refs.canvas;
+    ctx = canvas.getContext("2d", {
+      desynchronized: true
+    });
+    ctx.lineCap = "round";
+  }
+};
